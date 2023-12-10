fbbd54557d3751de6e0e0c8abb9469cb602ae86a
Prapto Mark <51514572+Praptomark@users.noreply.github.com>
2023-12-10 19:58:34 +0600

    fixed bug for showing multiple schedule in home

---
+++ src/lib/ongoing_movie_card.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let movie_poster = "/pexels-pavel-danilyuk-7234294.jpg"
+    export let movie_title = "Some Movie"
+</script>
+
+<div class="snap-center flex flex-col gap-1 w-[11rem] h-[17rem]">
+    <a href="/forAll/schedules" class="rounded-lg w-full h-full overflow-hidden">
+        <img src="http://127.0.0.1:8000/{movie_poster}" alt="movie poster" class="rounded-lg w-full h-full hover:scale-105 hover:transition hover:duration-300 hover:ease-in-out object-cover object-center">
+    </a>
+    <p class="font-opensans font-medium">{movie_title}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,40 @@
 <script>
-    import MovieCard from "$lib/movie_card.svelte";
-    
-    async function upcoming_movies(){
-        const response = await fetch("http://127.0.0.1:8000/api/movies/")
-        const upcoming_movie_data = response.json()
-        return (upcoming_movie_data)
+    import UpcommingMovieCard from "$lib/upcomming_movie_card.svelte";
+    import OngoingMovieCard from "$lib/ongoing_movie_card.svelte";
+
+    async function upcoming_movies() {
+        const response = await fetch("http://127.0.0.1:8000/api/movies/");
+        const upcoming_movie_data = response.json();
+        return upcoming_movie_data;
     }
 
-    async function ongoing_movies(){
-        const response = await fetch("http://127.0.0.1:8000/api/schedules/")
-        const ongoing_movie_data = response.json()
-        return (ongoing_movie_data)
+    async function ongoing_movies() {
+        const response = await fetch("http://127.0.0.1:8000/api/schedules/");
+        const ongoing_movie_data = await response.json();
+
+        // Use a Set to keep track of unique movie titles
+        const uniqueMovieTitles = new Set();
+
+        // Filter the ongoing_movie_data to include only one schedule per unique movie title
+        const filteredOngoingMovies = ongoing_movie_data.filter((schedule) => {
+            const title = schedule.movie.title;
+
+            // Check if the movie title is already in the Set
+            if (!uniqueMovieTitles.has(title)) {
+                // If not, add it to the Set and return true to include this schedule
+                uniqueMovieTitles.add(title);
+                return true;
+            }
+
+            // If the movie title is already in the Set, return false to exclude this schedule
+            return false;
+        });
+
+        return filteredOngoingMovies;
     }
-    console.log(upcoming_movies())
-    console.log(ongoing_movies())
+
+    console.log(upcoming_movies());
+    console.log(ongoing_movies());
 </script>
 
 <main>
@@ -46,10 +67,14 @@
         <div class="flex items-center h-[20rem] px-3 gap-5">
             {#await upcoming_movies()}
                 <h1 class="font-nunito text-center">Loading Movies...!!!</h1>
-            {:then upcoming_movie_data} 
+            {:then upcoming_movie_data}
                 {#each upcoming_movie_data as movie}
                     {#if movie.upcoming == true}
-                        <MovieCard movie_title={movie.title} movie_poster={movie.poster} movie_link={movie.movie_info_link}/>
+                        <UpcommingMovieCard
+                            movie_title={movie.title}
+                            movie_poster={movie.poster}
+                            movie_link={movie.movie_info_link}
+                        />
                     {/if}
                 {/each}
             {/await}
@@ -57,18 +82,19 @@
     </div>
 
     <div class="w-full flex flex-col items-center justify-center h-10 gap-1">
-        <h1 class="font-opensans font-medium cursor-default">
-            Ongoing Movies
-        </h1>
+        <h1 class="font-opensans font-medium cursor-default">Ongoing Movies</h1>
         <div class="h-0 w-20 border border-black" />
     </div>
     <div class="flex items-center gap-3 overflow-x-auto snap-x">
         <div class="flex items-center h-[20rem] px-3 gap-5">
             {#await ongoing_movies()}
                 <h1 class="font-nunito text-center">Loading Movies...!!!</h1>
-            {:then ongoing_movie_data} 
-                {#each ongoing_movie_data as film}
-                    <MovieCard movie_title={film.movie.title} movie_poster={film.movie.poster} movie_link={film.movie.movie_info_link}/>
+            {:then filteredOngoingMovies}
+                {#each filteredOngoingMovies as film}
+                    <OngoingMovieCard
+                        movie_title={film.movie.title}
+                        movie_poster={film.movie.poster}
+                    />
                 {/each}
             {/await}
         </div>
